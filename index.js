e8bd2665dc396dc22deb54ed65471f5b540dd8d0
danieldaudu65 <danieldaudu65@gmail.com>
2025-12-01 19:16:15 +0100

    first push of the backedn code

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // MongoDB connection
